d4ddee2d1eeb5bead4fa63d659d0a871d3d70f4b
sridhar02 <kattasridhar02@gmail.com>
2024-10-22 09:00:49 +0530

    📦 NEW: add search input & state

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,18 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [searchText, setSearchText] = useState("");
+
   return (
     <div className={styles.container}>
       <h1 className={styles.heading}> Live Weather App</h1>
+      <input
+        className={styles.searchBox}
+        type="text"
+        placeholder="Enter city name, like hyderabad or london"
+        value={searchText}
+        onChange={(e) => handleInputChange(e.target.value)}
+      />
     </div>
   );
 }
